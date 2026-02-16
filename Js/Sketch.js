a787d91f2fab46356818739dcd034d200147cefb
JaredSarabia01 <L24170739@culiacan.tecnm.mx>
2026-02-16 10:21:14 -0700

    Add files via upload

---
+++ Js/Sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(800, 500);
+  background(180);
+}
+
+function draw() {
+  stroke(0);
+  line(width/2, 0, width/2, height);
+  line(0,height/2, width, height/2);
+  line(width,0, 0, height);
+  line(0,0, width, height);
+  fill(0, 0, 255);
+  noStroke();
+  circle(width/2, height/2, 50);
+
+}
